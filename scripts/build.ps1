b4cbe93401eb7715072cba2efd025ebff0a50db0
merc1er <corentin@mercier.link>
2024-10-09 21:09:22 +0700

    Build a GUI with QT (#1)
    * Create trainer GUI
    
    * Set window title
    
    * Add mock god mode button
    
    * Integrate Nuitka to build an exe

---
+++ scripts/build.ps1
@@ -0,0 +1,1 @@
+nuitka --standalone --onefile --windows-console-mode=disable --enable-plugin=pyside6 --output-dir=dist src/main.py
--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+import sys
+
+from PySide6 import QtCore, QtWidgets, QtGui
 from pymem import Pymem  # type: ignore
 from pymem.exception import ProcessNotFound  # type: ignore
 
@@ -18,12 +21,56 @@ except ProcessNotFound:
 player = LocalPlayer(process)
 world = World(process)
 
-while True:
-    selection = input("1: Enable jump hack\n2: Disable jump hack\n")
-    if selection == "1":
-        world.jump_higher()
-    elif selection == "2":
-        world.disable_jump_higher()
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        # Set up the window.
+        self.setWindowTitle("p1nkrat's AssaultCube trainer")
+        self.setWindowIcon(QtGui.QIcon("logo.png"))
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.text = QtWidgets.QLabel(
+            "p1nkrat's AssaultCube trainer", alignment=QtCore.Qt.AlignCenter
+        )
+        self.layout.addWidget(self.text)
+
+        # Jump hack.
+        self.jump_hack_enabled = False
+        self.jump_hack_button = QtWidgets.QPushButton("🦘 Enable Jump Hack")
+        self.jump_hack_button.clicked.connect(self.enable_jump_hack)
+        self.layout.addWidget(self.jump_hack_button)
+
+        # God mode.
+        self.god_mode_enabled = False
+        self.god_mode_button = QtWidgets.QPushButton("🦸 Enable God Mode")
+        self.god_mode_button.clicked.connect(self.enable_god_mode)
+        self.layout.addWidget(self.god_mode_button)
+
+    @QtCore.Slot()
+    def enable_jump_hack(self) -> None:
+        if not self.jump_hack_enabled:
+            world.enable_jump_hack()
+            self.jump_hack_enabled = True
+            self.text.setText("Jump hack enabled.")
+        else:
+            world.disable_jump_hack()
+            self.jump_hack_enabled = False
+            self.text.setText("Jump hack disabled.")
+
+    @QtCore.Slot()
+    def enable_god_mode(self) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(400, 300)
+    widget.show()
+
+    sys.exit(app.exec())
 
 # player.set_all_ammo(1337)
 # player.set_heatlh(1337)
